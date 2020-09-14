750a100a2d7e52f19a2f1c12682316886a783d6a
Merlin Webster <mjftwebster@gmail.com>
2020-09-14 08:41:25 +0100

    Add initial pip package setup config

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+
+setuptools.setup(
+    name='brewing-inventory',
+    version='0.0.0',
+    author='Merlin Webster',
+    author_email='mjftwebster@gmail.com',
+    description='Brewing inventory management system',
+    url='https://github.com/mjftw/pywebapp-demo',
+    package_dir={'': 'src'},
+    packages=['brewinv'],
+    python_requires='>=3.6',
+    install_requires=[]
+)
